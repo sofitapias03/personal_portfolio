90e08a57a099d1905780e08b59404cf9cdd33aed
sofia tapias <sofia.tapiasmontana@ucalgary.ca>
2024-06-06 21:36:02 -0600

    fixed the navbar that was overstepping on the text and thus was not allowing for correct scrolling, specially bad on smaller screens, but now its fixed

--- tapias_portfolio/src/Components/navBar.js
+++ tapias_portfolio/src/Components/navBar.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../pages/MobileNavBar.css';
 
+
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
@@ -10,33 +11,29 @@ const NavBar = () => {
   };
 
   return (
-    <div className="nav-container">
-  <div className="nav-menu">
-  <div className="menu-text">Menu</div>
-  <button className="nav-toggle" onClick={toggleNav} style={{ fontSize: '80px', color:'#efe49f', marginRight:'-150px' }}>
-    &#9776; {/* Hamburger icon */}
-  </button>
-</div>
-
-  <div className={`nav-links ${isOpen ? 'active' : ''}`}>
-    <Link to="/aboutMe" className="nav-item" onClick={toggleNav}>
-      About Me
-    </Link>
-    <Link to="/Projects" className="nav-item" onClick={toggleNav}>
-      Projects
-    </Link>
-    <Link to="/Contact" className="nav-item" onClick={toggleNav}>
-      Contact
-    </Link>
-    <Link to="/" className="nav-item" onClick={toggleNav}>
-      Home
-    </Link>
-  </div>
-</div>
-
+    <div className="nav-wrapper">
+      <div className="nav-menu">
+        <div className="menu-text">Menu</div>
+        <button className="nav-toggle" onClick={toggleNav}>
+          &#9776; {/* Hamburger icon */}
+        </button>
+        <div className={`nav-links ${isOpen ? 'active' : ''}`}>
+          <Link to="/aboutMe" className="nav-item" onClick={toggleNav}>
+            About Me
+          </Link>
+          <Link to="/Projects" className="nav-item" onClick={toggleNav}>
+            Projects
+          </Link>
+          <Link to="/Contact" className="nav-item" onClick={toggleNav}>
+            Contact
+          </Link>
+          <Link to="/" className="nav-item" onClick={toggleNav}>
+            Home
+          </Link>
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default NavBar;
-
-
